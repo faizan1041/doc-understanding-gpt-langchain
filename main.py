b152baa6fb7444a45a963c274facaf6e696c0d55
Faizan Ali <faizanalisoomro1988@gmail.com>
2023-11-28 12:48:28 +0500

    Fixed document upload Fais vector

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import OpenAI
-from local_vector_store_v2 import ingest_docs
+from local_vector_store import ingest_docs
 from config.config import *
 import openai
 from urllib.parse import urlparse
 import os
 from pathlib import Path  # Import pathlib
+from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+
+from langchain.llms.base import LLM
+from langchain.llms.utils import enforce_stop_tokens
+from langchain.llms import GooglePalm
+
+from config.config import *
+
+
+template = "Hey, Google here. So you want me to analyze a document. Please upload it!"
 
 class DocumentAnalyzer:
     def __init__(self):
@@ -19,21 +34,31 @@ class DocumentAnalyzer:
         self.conversation_history = []
         self.file_path = None
         self.chat_history = []
-        self.document_names = set()  # Use a set for document names
-        self.initialize_apis()  # Initialize APIs here
+        self.document_names = set()
+        self.initialize_apis()
+        self.selected_api = None
+        print("Server started... ")
 
     def initialize_apis(self):
-        # Initialize APIs and models here, e.g., ChatOpenAI
         self.chat = ChatOpenAI(verbose=True, temperature=0)
-        # ... Initialize other APIs and models ...
+        self.fact_llm = GooglePalm(temperature=0)
+
 
     def run_llm(self, query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
         docsearch = ingest_docs(self.file_path, self.index_type)
-        qa = ConversationalRetrievalChain.from_llm(
-            llm=self.chat,
-            retriever=docsearch.as_retriever(),
-        )
-        return qa({"question": query, "chat_history": chat_history})
+        if any(keyword in query.lower() for keyword in {"google", "google palm", "Google"}):
+            qa = ConversationalRetrievalChain.from_llm(
+                llm=self.fact_llm,  
+                retriever=docsearch.as_retriever(),
+            )
+            return qa({"question": query, "chat_history": chat_history})
+        else:
+            qa = ConversationalRetrievalChain.from_llm(
+                llm=self.chat,  
+                retriever=docsearch.as_retriever(),
+            )
+            return qa({"question": query, "chat_history": chat_history})
+    
 
     def analyze_document(self, query):
         generated_response = self.run_llm(query, self.chat_history)
@@ -41,49 +66,73 @@ class DocumentAnalyzer:
         self.chat_history.append((query, generated_response["answer"]))
         return formatted_response
 
+
     def process_message(self, update, context):
         user_message = update.message.text
+        keywords = {"document", "documents", "file", "analyze", "analyse", "report"}
 
         if self.analyzing_document:
+            print("Analyzing document: -------> ")
+            # Continue analyzing the document
             self.conversation_history.append({"role": "user", "content": user_message})
             response = self.analyze_document(user_message)
             self.conversation_history.append({"role": "assistant", "content": response})
             update.message.reply_text(response)
-        else:
-            keywords = {"document", "documents", "file", "analyze", "analyse", "report"}
-            if any(keyword in user_message.lower() for keyword in keywords):
+        elif any(keyword in user_message.lower() for keyword in keywords) and not self.document_uploaded:
                 self.analyzing_document = True
-                self.document_uploaded = False  # Reset the document_uploaded flag
+                self.document_uploaded = False
                 update.message.reply_text("You want to analyze a document. Please upload the document.")
-            else:
-                self.conversation_history.append({"role": "user", "content": user_message})
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=self.conversation_history
-                )
-                assistant_reply = response.choices[0].message["content"]
-                update.message.reply_text(assistant_reply)
-                self.conversation_history.append({"role": "assistant", "content": assistant_reply})
+        else:
+            print("I did not see a document so Im here")
+            # Continue regular conversation
+            self.conversation_history.append({"role": "user", "content": user_message})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.conversation_history
+            )
+            assistant_reply = response.choices[0].message["content"]
+            update.message.reply_text(assistant_reply)
+            self.conversation_history.append({"role": "assistant", "content": assistant_reply})
+
+
 
     def download_and_analyze(self, update, context):
+        if update.message.document:
+            print("---------------- Document Found ------------------")
+            file_id = update.message.document.file_id
+            print(f"file ID: {file_id}")
+            print("--------------------------------------------------")
+            document = context.bot.get_file(file_id)
+            self.document_names.add(document)
+            file_extension = Path(document.file_path).suffix.lower()
+            if file_extension in ['.pdf', '.docx', '.txt']:
+                parsed_url = urlparse(document.file_path)
+                filename = os.path.basename(parsed_url.path)
+                self.file_path = f'{OUTPUT_DIR}/{filename}'
+                document.download(self.file_path)
+                self.document_uploaded = True
+                docsearch = ingest_docs(self.file_path, self.index_type)
+                response = "You uploaded a document. Please ask anything about it."
+                self.analyzing_document = True
+            else:
+                response = "Unsupported file format. Please upload a PDF, DOCX, or TXT document."
+                    
+            self.conversation_history.append({"role": "assistant", "content": response})
+            update.message.reply_text(response)
         if self.analyzing_document and context.bot.get_file(update.message.document):
             document = context.bot.get_file(update.message.document)
-            if document in self.document_names:
-                update.message.reply_text("This document already exists in the Database. Please ask a question about the document.")
-                return
+            self.document_names.add(document)
+            file_extension = Path(document.file_path).suffix.lower()
+            if file_extension in ['.pdf', '.docx', '.txt']:
+                parsed_url = urlparse(document.file_path)
+                filename = os.path.basename(parsed_url.path)
+                self.file_path = f'{OUTPUT_DIR}/{filename}'
+                document.download(self.file_path)
+                self.document_uploaded = True
+                update.message.reply_text("Document uploaded. Ask away.")
             else:
-                self.document_names.add(document)
-                file_extension = Path(document.file_path).suffix.lower()  # Use pathlib
-                if file_extension in ['.pdf', '.docx', '.txt']:
-                    parsed_url = urlparse(document.file_path)
-                    filename = os.path.basename(parsed_url.path)
-                    self.file_path = f'{OUTPUT_DIR}/{filename}'  # Store the file path in the instance variable
-                    document.download(self.file_path)
-                    self.document_uploaded = True
-                    update.message.reply_text("Document uploaded. Please ask a question about the document.")
-                else:
-                    update.message.reply_text("Unsupported file format. Please upload a PDF, DOCX, or TXT document.")
-
+                update.message.reply_text("Unsupported file format. Please upload a PDF, DOCX, or TXT document.")
+ 
 def main():
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
@@ -98,6 +147,7 @@ def main():
 
     updater.start_polling()
     updater.idle()
+    
 
 if __name__ == "__main__":
     main()
